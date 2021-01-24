cbf9d2326f41878fd55fdf131e17625dbae90078
John Ng <chuchengng@gmail.com>
2021-01-24 21:16:58 +0800

    Replaced some code of Hell Salamander

--- beys/raws/HellSalamander.js
+++ beys/raws/HellSalamander.js
@@ -1,33 +1,33 @@
 const bcworkshop = require("bcworkshop");
 
 function SRHSRequirement(acted, victim, logger){
-     return acted.sp >= 3 && acted.bey.type == Attack;
+     return acted.sp >= 3 && acted.bey.type == "Attack";
 }
 
 function SRHS(acted, victim, logger){
      victim.hp -= (50 + .7 * acted.lvl);
      acted.stamina -= (1 + .008 * acted.lvl);
-     logger.add(`[${acted.username}] Hell Salamander used **Sword Rebellion Hell Slash**!`)
+     logger.add(`[${acted.username}] Hell Salamander used **Sword Rebellion Hell Slash**!`);
 }
 
 const SwordRebellionHellSlash = new bcworkshop.Special("Sword Rebellion Hell Slash", SRHSRequirement, SRHS);
 
 
 function RCHRequirement(acted, victim, logger){
-     return acted.sp >= 3 && acted.bey.type == Defense;
+     return acted.sp >= 3 && acted.bey.type == "Defense";
 }
 
 function RCH(acted, victim, logger){
       acted.stamina = (1 + .018 * acted.lvl);
       victim.atk = (victim.atk = (victim.atk/100 * (50 - .2 * acted.lvl)));
-      logger.add(`[${acted.username}] Hell Salamander used **Raging Crimson Hellfire**!`)
+      logger.add(`[${acted.username}] Hell Salamander used **Raging Crimson Hellfire**!`);
 }
 
 const RagingCrimsonHellfire = new bcworkshop.Special("Raging Crimson Hellfire", RCHRequirement, RCH);
 
 
 function BRRequirement(acted, victim, logger){
-     return acted.sp >= .5 && acted.move == fight;
+     return acted.sp >= .5 && acted.move == "fight";
 }
 
 function BR(acted, victim, logger){
@@ -45,7 +45,7 @@ function BR(acted, victim, logger){
 const BurningResonance = new bcworkshop.Passive("Burning Resonance", BRRequirement, BR, 0);
 
 function FIFRequirements(acted, victim, logger){
-      return acted.sp >= 3 && acted.bey.type == Defense && victim.move == fight;
+      return acted.sp >= 3 && acted.bey.type == "Defense" && victim.move == "fight";
 }
 
 function FIF(acted, victim, logger){
@@ -58,7 +58,7 @@ function FIF(acted, victim, logger){
 const ForgedInFire = new bcworkshop.Passive("Forged In Fire", FIFRequirements, FIF, 60);
 
 function UTRequirements(acted, victim, logger){
-      return acted.sp >= 3 && acted.bey.type == Attack && victim.move == fight;
+      return acted.sp >= 3 && acted.bey.type == "Attack" && victim.move == "fight";
 }
 
 function UT(acted, victim, logger){
@@ -68,14 +68,14 @@ function UT(acted, victim, logger){
      }
 }
 
-const UntouchableFlame = new bcworkshop.Passive("Untouchable Flame", UTRequirements, UT, 60)
+const UntouchableFlame = new bcworkshop.Passive("Untouchable Flame", UTRequirements, UT, 60);
 
 function AttackRequirement(acted, victim, logger){
      return acted.bey.SalamanderMode == 1;
 }
 
 function AttackM(acted, victim, logger){
-     acted.bey.type = Attack
+     acted.bey.type = "Attack";
 }
 
 const AttackMode = new bcworkshop.Mode("Attack Mode", AttackRequirement, AttackM);
@@ -86,7 +86,7 @@ function DefenseRequirement(acted, victim, logger){
 }
 
 function DefenseM(acted, victim, logger){
-     acted.bey.type = Defense
+     acted.bey.type = "Defense";
 }
 
 const DefenseMode = new bcworkshop.Mode("Defense Mode", DefenseRequirement, DefenseM);
